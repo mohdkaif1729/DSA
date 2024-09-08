26f35f5cde545ceb8c8359457ef9f70ae3c863fd
Mohd Kaif <mkmohdkaif263@gmail.com>
2024-09-08 11:45:48 +0530

    even numbers

---
+++ 06_recursion/04_evenNumbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void printEvenNumber(int num) {
+  if (num == 0) {
+    return;
+  }
+  printEvenNumber(num - 1);
+  cout<<2*num<<endl;
+  return;
+}
+
+int main() {
+  int num;
+  cout<<"Enter a number: ";
+  cin>>num;
+
+  printEvenNumber(num);
+
+  return 0;
+}
