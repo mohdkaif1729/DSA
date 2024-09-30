1499d836ccc997e8421df19574f58b6b4eae5ae8
Mohd Kaif <mohdkaif1729>
2024-09-30 17:45:05 +0530

    insertion using recursion

---
+++ 07_linkedList/02_singlyLinkedListInsertionUsingRecursion.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int data) {
+      this->data = data;
+      this->next = NULL;
+      this->next = NULL;
+    }
+};
+
+void displayList(Node *head) {
+  while(head) {
+    cout << head->data << " ";
+    head = head->next;
+  }
+  cout << endl;
+  return;
+}
+
+
+void insertAtEnd(Node *head, Node *newNode) {
+  if (head->next == NULL) {
+    head->next = newNode;
+    return;
+  }
+
+  insertAtEnd(head->next, newNode);
+  return;
+}
+
+
+int main() {
+  // ********************* insertion in end ****************************************
+  int arr[] = {3, 1, 5, 2, 9, 8, 0};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  Node *head = NULL;
+
+  for (int i = 0; i < n; i++) {
+    if (head == NULL) {
+      Node *newNode = new Node(arr[i]);
+      head = newNode;
+    }
+    else {
+      Node *newNode = new Node(arr[i]);
+      insertAtEnd(head, newNode);
+    }
+  }
+  displayList(head);
+
+
+
+
+  // ********************* insertion in start ****************************************
+  // int arr[] = {3, 1, 5, 2, 9, 8, 0};
+  // int n = sizeof(arr) / sizeof(arr[0]);
+  // Node *head = NULL;
+
+  // for (int i = n - 1; i >= 0; i--) {
+  //   if (head == NULL) {
+  //     Node *newNode = new Node(arr[i]);
+  //     head = newNode;
+  //   }
+  //   else {
+  //     Node *newNode = new Node(arr[i]);
+  //     newNode->next = head;
+  //     head = newNode;
+  //   }
+  // }
+  // displayList(head);
+  return 0;
+}
